af21f7da40c8b4edad530551f6dc3a0e6867d0c9
Jason Ledon <jledon@amazon.com>
2023-08-06 16:17:44 -0700

    starting day 4, cant find how to use Range as a type in a struct

---
+++ 2022/rust/day-04/src/main.rs
@@ -0,0 +1,47 @@
+use std::ops::Range;
+
+fn main() {
+    let data = read_data();
+    println!("{:?}", part1(&data));
+    println!("{:?}", part2(&data));
+}
+
+fn get_file_name() -> String {
+    let cwd = std::env::current_dir().ok().unwrap();
+    let cwd_str = cwd.to_str().unwrap();
+
+    let re = regex::Regex::new(r"(^.*)/(\d{4})/.*/day-(\d+).*$").unwrap();
+    let cap = re.captures(cwd_str).unwrap();
+    let leading_dir = cap.get(1).unwrap().as_str();
+    let year = cap.get(2).unwrap().as_str();
+    let day = cap.get(3).unwrap().as_str().trim_start_matches('0');
+
+    let path = std::path::Path::new(".")
+                    .join(leading_dir)
+                    .join(year)
+                    .join("input-files")
+                    .join(format!("adventofcode.com_{year}_day_{day}_input.txt"));
+    assert!(path.exists());
+    let path_str = path.to_str().unwrap();
+
+    return String::from(path_str);
+    
+}
+
+fn read_data() -> String {
+    let filepath = get_file_name();
+    return std::fs::read_to_string(filepath).ok().unwrap();
+}
+
+struct Ranges {
+    first: Range,
+    second: Range
+}
+
+fn part1(data: &String) /* -> u32 */ {
+
+}
+
+fn part2(data: &String) /* -> u32 */ {
+
+}
