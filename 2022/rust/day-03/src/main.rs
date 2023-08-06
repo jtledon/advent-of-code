6b990124bb94422814786cd0768b675fded7b1f5
Jason Ledon <jledon@amazon.com>
2023-08-06 16:02:39 -0700

    Got part 2 working on day 3 in rust

--- 2022/rust/day-03/src/main.rs
+++ 2022/rust/day-03/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let data = read_data();
-    println!("{:?}", part1(data));
+    println!("{:?}", part1(&data));
+    println!("{:?}", part2(&data));
 }
 
 fn get_file_name() -> String {
@@ -30,7 +31,7 @@ fn read_data() -> String {
     return std::fs::read_to_string(filepath).ok().unwrap();
 }
 
-fn part1(data: String) -> u32 {
+fn part1(data: &String) -> u32 {
     data
         .lines()
         .map(|line| {
@@ -51,3 +52,58 @@ fn part1(data: String) -> u32 {
         })
         .sum::<u32>()
 }
+
+// fn part2(data: &String) -> u32 {
+//     for group in 
+//     data
+//         .split("\n\n")
+//         .map(|group| {
+//             let lines = group.lines();
+//             let sets = lines.map(|rucksack| std::collections::HashSet::from_iter(rucksack.chars()));
+//             println!("{sets:?}");
+//             let intersect: HashSet<char, _> = 
+//                 sets
+//                 .reduce(|a: HashSet<char, _>, b: HashSet<char, _>| 
+//                         a.intersection(&b)
+//                         .cloned()
+//                         .collect::<HashSet<char>>())
+//                 .unwrap();
+//             println!("{intersect:?}");
+//             intersect.iter().map(|char| 
+//                         match char {
+//                             'a'..='z' => (*char as u32) - 'a' as u32 + 1,
+//                             'A'..='Z' => (*char as u32) - 'A' as u32 + 26 + 1,
+//                             _ => todo!()
+//                         })
+//             .sum::<u32>()
+//         })
+//         .sum::<u32>()
+// }
+
+fn part2(data: &String) -> u32 {
+    let mut res: u32 = 0;
+    let group = data.lines().collect::<Vec<&str>>();
+    let chunks = group.chunks(3);
+    for chunk in chunks {
+        let mut sets = vec!();
+        for line in chunk {
+            let set = std::collections::HashSet::<_>::from_iter(line.chars());
+            sets.push(set.clone());
+        }
+        let intersects = sets
+            .iter()
+            .skip(1)
+            .fold(sets[0].clone(), |acc, hs| {
+                acc.intersection(hs).cloned().collect()
+            });
+        let ints = intersects.iter().map(|char| 
+                    match char {
+                        'a'..='z' => (*char as u32) - 'a' as u32 + 1,
+                        'A'..='Z' => (*char as u32) - 'A' as u32 + 26 + 1,
+                        _ => todo!()
+                    });
+        let int = ints.sum::<u32>();
+        res = res + int;
+    }
+    return res;
+}       
