53a2f89639d9b07c0496e77ed6d8aaec8df72c69
Jason Ledon <jtledon@gmail.com>
2025-12-02 22:08:15 -0800

    feat: advent-of-code 2025 day1 both parts completed

---
+++ 2025/golang/day1/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	// "io"
+	// "bytes"
+)
+
+func main() {
+	fmt.Printf("%s\n\n", "main() function being invoked!")
+	defer fmt.Println("\nCompleted main() function")
+
+	filename := "../../input-files/day1.txt"
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	// data, err := io.ReadAll(file)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// part1(reader)
+	part2(reader)
+}
+
+func part1(reader *bufio.Reader) {
+	currPosition := 50
+	maxPosition := 100
+	zeroCount := 0
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		direction := line[0]
+		movementAmount, err := strconv.Atoi(strings.TrimSpace(line[1:]))
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("%d", currPosition)
+		switch direction {
+		case 'L':
+			currPosition = (currPosition - movementAmount) % maxPosition
+			if currPosition < 0 { currPosition += maxPosition }
+			fmt.Printf(" - %d = ", movementAmount)
+		case 'R':
+			currPosition = (currPosition + movementAmount) % maxPosition
+			fmt.Printf(" + %d = ", movementAmount)
+		}
+		fmt.Println(currPosition)
+
+		if currPosition == 0 { zeroCount++ }
+	}
+	fmt.Printf("Part1 Answer: %d\n", zeroCount)
+}
+
+func part2(reader *bufio.Reader) {
+	currPosition := 50
+	maxPosition := 100
+	zeroCount := 0
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		direction := line[0]
+		movementAmount, err := strconv.Atoi(strings.TrimSpace(line[1:]))
+		if err != nil {
+			panic(err)
+		}
+
+		// for i := 0; i < movementAmount; i++ {
+		for range movementAmount {
+			switch direction {
+			case 'L':
+				currPosition--
+				if currPosition < 0 { currPosition += maxPosition }
+			case 'R':
+				currPosition++
+				currPosition = currPosition % maxPosition
+			}
+			if currPosition == 0 { zeroCount++ }
+		}
+
+	}
+	fmt.Printf("Part2 Answer: %d\n", zeroCount)
+}
