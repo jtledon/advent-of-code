f132352daa09c0e28b6f682dc8c3362595d16022
Jason Ledon <jledon@amazon.com>
2025-12-20 14:35:09 -0500

    feat: setup remaining days that need to be solved

--- 2025/golang/day11/main_test.go
+++ 2025/golang/day11/main_test.go
@@ -8,10 +8,20 @@ import (
 // go test ./...
 
 var sampleInput string = `
+aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out
 `
 
 func TestPart1(t *testing.T) {
-	expected := 0
+	expected := 5
 	result := Part1(sampleInput)
 	if expected != result {
 		t.Fatalf("Expected: %d, Received: %d", expected, result)
--- 2025/golang/day12/main_test.go
+++ 2025/golang/day12/main_test.go
@@ -8,10 +8,43 @@ import (
 // go test ./...
 
 var sampleInput string = `
+0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2
 `
 
 func TestPart1(t *testing.T) {
-	expected := 0
+	expected := 2
 	result := Part1(sampleInput)
 	if expected != result {
 		t.Fatalf("Expected: %d, Received: %d", expected, result)
